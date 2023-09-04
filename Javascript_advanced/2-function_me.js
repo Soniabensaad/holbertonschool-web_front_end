ef1c08f2d1131358ae38221c965bd4d1f91dd795
sonia <https://github.com/Soniabensaad>
2023-09-04 19:32:41 +0200

    js

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,12 @@
+function welcomeMessage(fullName) {
+    alert("Welcome " + fullName);
+}
+
+
+var Guillaume = "Guillaume";
+var Alex = "Alex";
+var Fred = "Fred";
+
+welcomeMessage(Guillaume);
+welcomeMessage(Alex);
+welcomeMessage(Fred);
