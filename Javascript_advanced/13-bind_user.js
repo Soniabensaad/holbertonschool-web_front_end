2c85503442817f60a5cffa3b914090a5c9af5b53
sonia <https://github.com/Soniabensaad>
2023-09-07 18:44:36 +0200

    Javascript_advanced

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,16 @@
+const user ={
+    hobby: "Calligraphy",
+    favoriteSport: "Hockey",
+    astrologicalSign: "Aries",
+    firstName: "Buillaume",
+    lastName: "Ialva",
+    location: "Telaviv",
+    occupation: "Engineer"
+
+}
+
+function logWelcomeUser(welcomeString){
+    console.log(`${welcomeString},  ${user.firstName}. Your occupation is: ${user.occupation}`)
+}
+const bindLogWelcomeUser = logWelcomeUser.bind(user)
+bindLogWelcomeUser('Hello')
